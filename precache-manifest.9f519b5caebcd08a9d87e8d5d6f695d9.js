e7c6366f384bb108c5f5e50777e4d13eaa999d7b
Pilar Argudo <pargudo@disid.com>
2019-04-23 10:54:56 +0200

    Updates

---
+++ precache-manifest.9f519b5caebcd08a9d87e8d5d6f695d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef408d7a1e4c1256690",
+    "url": "/react-films/static/js/runtime~main.f7f8d442.js"
+  },
+  {
+    "revision": "73388305359de0bac867",
+    "url": "/react-films/static/js/main.2447a1e1.chunk.js"
+  },
+  {
+    "revision": "cc9f8ea6cc653bbff3d5",
+    "url": "/react-films/static/js/2.6f1a749d.chunk.js"
+  },
+  {
+    "revision": "73388305359de0bac867",
+    "url": "/react-films/static/css/main.8659636d.chunk.css"
+  },
+  {
+    "revision": "1c1e8b33f361fc9edf5b50cf0e477260",
+    "url": "/react-films/index.html"
+  }
+];
